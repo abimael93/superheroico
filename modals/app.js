d9522972548ad88ed74a48430355d77ebefaf863
cesar <kyele936@gmail.com>
2015-02-02 15:09:40 -0600

    ejemplo de modales con json y ui-bootstrap

---
+++ modals/app.js
@@ -0,0 +1,86 @@
+var app = angular.module('app', ['ui.bootstrap','ngRoute','ngResource']);
+ 
+/**
+* configuración de nuestra aplicación
+* @param $routeProvider
+*/
+app.config(['$routeProvider', function($routeProvider)
+{
+    $routeProvider.when("/home", {
+        templateUrl: "templates/home.html",
+        controller: "homeController"
+    })
+    .otherwise({ redirectTo : "/"});
+}]);
+ 
+ 
+ 
+/**
+* @description Controlador home
+* @param $scope
+* @param $modal
+*/
+app.controller('homeController', ['$scope','$modal','$http','dataResource', function($scope,$modal,$http,dataResource)
+{    
+    $http.get('data.json').success(function (data) {
+        //Convert data to array.
+        //datos lo tenemos disponible en la vista gracias a $scope
+        $scope.datos = data;
+    });
+    //datosResource lo tenemos disponible en la vista gracias a $scope
+    $scope.datosResource = dataResource.get();
+    /**
+    * abre la modal
+    */
+    $scope.openModal = function (size)
+    {
+        var modalInstance = $modal.open({
+            templateUrl: 'myModal.html',
+            controller: 'myModalController',
+            size: size,
+            resolve: {
+                Items: function() //scope del modal
+                {
+                      return $scope.datos;
+                }
+            }
+        });
+    }
+}]);
+ 
+/**
+* @description Controlador para la modal
+* @param $scope
+* @param $modalInstance
+* @param Items
+*/
+app.controller('myModalController', ['$scope','$modalInstance','Items', function($scope, $modalInstance,Items)
+{
+    $scope.items = Items;
+ 
+    $scope.save = function (param)
+    {
+        console.log(param)
+    };
+ 
+    $scope.cancel = function ()
+    {
+        $modalInstance.dismiss('cancel');
+    };
+}]);
+ 
+//con dataResource inyectamos la factoría
+app.controller("appController", function ($scope, $http, dataResource) {
+    //hacemos uso de $http para obtener los datos del json
+    
+})
+ 
+//de esta forma tan sencilla consumimos con $resource en AngularJS
+app.factory("dataResource", function ($resource) {
+    return $resource("data.json", //la url donde queremos consumir
+        {}, //aquí podemos pasar variables que queramos pasar a la consulta
+        //a la función get le decimos el método, y, si es un array lo que devuelve
+        //ponemos isArray en true
+        { get: { method: "GET", isArray: true }
+    })
+})
